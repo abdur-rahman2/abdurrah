6b9d4798b634edc528de6e668c786ad466a11ad3
Abdur Rahman <abdurrahmantuhin3333@gmail.com>
2025-10-24 23:18:22 +0600

    Create DesktopNotifier.py

---
+++ DesktopNotifier.py
@@ -0,0 +1,5 @@
+from win10toast import ToastNotifier
+Toast = ToastNotifier()
+Toast.show_toast("Learning Time","Hello abdur rahman it's time to learn something new",duration=5)
+Toast.show_toast("Learning Time","Hello abdur rahman it's time to learn something new",duration=2)
+Toast.show_toast("Learning Time","Hello abdur rahman it's time to learn something new",duration=1)
